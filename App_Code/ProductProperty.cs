1858c8d1cc2eafd5869af47ed1b0a2e92e7d130f
clox <ogggan@gmail.com>
2013-02-07 01:19:21 +0100

    Start
    Kan skapa nya produkter i alla fall

---
+++ App_Code/ProductProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassName
+/// </summary>
+public class ProductProperty
+{
+        public string name;
+        public string displayName;
+        public string type;
+        public bool required;
+        public ProductProperty (string name, string displayName, string type, bool required=false) {
+            this.name=name;
+            this.displayName=displayName;
+            this.type=type;
+            this.required=required;
+    }
+}
